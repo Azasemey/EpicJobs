a41a9a533dfdc1d6d32ce93b28169aa15c9e85c3
Azasemey <azasemey@gmail.com>
2021-08-06 21:21:24 +0600

    fix: tabs now works as expected, added advantages and footer sections

--- script/main.js
+++ script/main.js
@@ -1,14 +1,27 @@
 "use strict";
 const gamesDescr = document.querySelectorAll(".game-tabs-content > div");
 const mainTabs = document.querySelectorAll(".game-tabs > button");
-function tabSwitcher(buttons, blocks) {
+const vacTabs = document.querySelectorAll(".vac-block > ul > li > button");
+const vacDescr = document.querySelectorAll(".descr-img");
+// console.log(vacTabs);
+function tabSwitcher(buttons, blocks, dataSet) {
   buttons.forEach((el) =>
     el.addEventListener("click", function (e) {
-      blocks.forEach((v) => v.classList.add("none"));
+      const active = e.target.closest("button");
+      blocks.forEach((v) => {
+        v.classList.add("none");
+        active.classList.remove("active");
+      });
+      buttons.forEach((v) => {
+        v.classList.remove("active");
+      });
       document
-        .getElementById(`${e.target.dataset.game}`)
+        .getElementById(`${e.target.dataset[dataSet]}`)
         .classList.remove("none");
+      active.classList.add("active");
     })
   );
 }
-tabSwitcher(mainTabs, gamesDescr);
+
+tabSwitcher(mainTabs, gamesDescr, "game");
+tabSwitcher(vacTabs, vacDescr, "vac");
