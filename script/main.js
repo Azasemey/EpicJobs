c60d80d14861c8533c377767cf9918402a6aec87
Azasemey <azasemey@gmail.com>
2021-08-07 12:22:07 +0600

    feat: adaptive design in process, js code fixed

--- script/main.js
+++ script/main.js
@@ -1,27 +1,58 @@
 "use strict";
 const gamesDescr = document.querySelectorAll(".game-tabs-content > div");
 const mainTabs = document.querySelectorAll(".game-tabs > button");
-const vacTabs = document.querySelectorAll(".vac-block > ul > li > button");
+const vacTabs = document.querySelectorAll("ul.vacancies > li > button");
 const vacDescr = document.querySelectorAll(".descr-img");
-// console.log(vacTabs);
-function tabSwitcher(buttons, blocks, dataSet) {
-  buttons.forEach((el) =>
-    el.addEventListener("click", function (e) {
-      const active = e.target.closest("button");
-      blocks.forEach((v) => {
-        v.classList.add("none");
-        active.classList.remove("active");
+const mobVac = document.querySelectorAll(".mob");
+const vacDiv = document.querySelector("div.game > div");
+function tabs() {
+  mainTabs.forEach((e) => {
+    e.addEventListener("click", function (el) {
+      const btnGame = el.target.closest("button");
+      const vac = document.querySelector(
+        `#${btnGame.dataset.game}> div > div > ul > li:nth-child(1) > button`
+      );
+      const vacM = document.querySelector(
+        `#mob-${btnGame.dataset.game}>  ul > li:nth-child(1) > button`
+      );
+      mainTabs.forEach((b) => b.classList.remove("active"));
+      btnGame.classList.add("active");
+      vacTabs.forEach((v) => v.classList.remove("active"));
+      vacDescr.forEach((d) => d.classList.add("none"));
+      vac?.classList.add("active");
+      vacM?.classList.add("active");
+      if (btnGame.dataset.game === "destiny") {
+        document.getElementById("destiny-all").classList.remove("none");
+      } else if (btnGame.dataset.game === "hs") {
+        document.getElementById("hs-all").classList.remove("none");
+      } else if (btnGame.dataset.game === "dota") {
+        document.getElementById("dota-all").classList.remove("none");
+      }
+      mainTabs.forEach((b) => {
+        if (b.classList.contains("active")) {
+          gamesDescr.forEach((v) => v.classList.add("none"));
+          mobVac.forEach((v) => v.classList.add("none"));
+          document.getElementById(b.dataset.game).classList.remove("none");
+          document
+            .getElementById(`mob-${b.dataset.game}`)
+            ?.classList.remove("none");
+          document.getElementById(vac?.dataset.vac)?.classList.remove("none");
+        }
       });
-      buttons.forEach((v) => {
-        v.classList.remove("active");
+    });
+  });
+  vacTabs.forEach((e) => {
+    e.addEventListener("click", function (e) {
+      const vac = e.target;
+      vacTabs.forEach((b) => b.classList.remove("active"));
+      vac.classList.add("active");
+      vacDescr.forEach((v) => v.classList.add("none"));
+      vacTabs.forEach((v) => {
+        if (v.classList.contains("active")) {
+          document.getElementById(v.dataset.vac).classList.remove("none");
+        }
       });
-      document
-        .getElementById(`${e.target.dataset[dataSet]}`)
-        .classList.remove("none");
-      active.classList.add("active");
-    })
-  );
+    });
+  });
 }
-
-tabSwitcher(mainTabs, gamesDescr, "game");
-tabSwitcher(vacTabs, vacDescr, "vac");
+tabs();
